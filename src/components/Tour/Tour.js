c8e2ca92c8a5b7f0b838c2dd538f1de9b3c3b8b7
Marichelle Bisuna <marichellebisuna@yahoo.com>
2019-02-19 11:59:43 +1100

    displaying all the data from the tourlist file

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -3,17 +3,18 @@ import './Tour.scss';
 
 export default class Tour extends Component {
   render() {
+      const{city, img, name, info}=this.props.tour;
     return (
       <article className="tour">
           <div className="img-container">
-            <img src="https://images.pexels.com/photos/1887629/pexels-photo-1887629.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""/>
+            <img src={img} alt=""/>
             <span className="close-btn"><i className="fas fa-window-close" /></span>
           </div>
           <div className="tour-info">
-            <h3>city</h3>
-            <h4>name</h4>
+            <h3>{city}</h3>
+            <h4>{name}</h4>
             <h5>info<span><i className="fas fa-caret-square-down"></i></span></h5>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, quia. Est itaque alias quis, quasi expedita labore! Repellendus commodi ad consectetur vitae corrupti. Nobis veniam quidem saepe veritatis iusto animi!</p>
+            <p>{info}</p>
           </div>
       </article>
     
